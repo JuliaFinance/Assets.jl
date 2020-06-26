305f361c91a1af22b311be882a0260917390bea2
Eric Forgy <Eric.Forgy@cavalre.com>
2020-06-25 19:56:59 -0700

    Remove `ListedStock` and add validation for `Cash`

--- src/Assets.jl
+++ src/Assets.jl
@@ -5,7 +5,6 @@ This package provides implementations of `Instrument` for various financial asse
 
 - `Cash`: which is based on a particular currency type, along with the minor unit.
 - `Stock`: which represents a general common stock.
-- `ListedStock`: which represents a common stock listed on an exchange.
 
 It also provides a specialized `Position` for `Cash` that uses the currency's minor unit.
 
@@ -20,12 +19,14 @@ module Assets
 using Currencies, FixedPointDecimals
 using Instruments: Instrument; import Instruments: Position, symbol, currency, unit, code, name
 
-export Cash, ListedStock
+export Cash, Stock
 
 """
 `Cash` is an implementation of `Instrument` represented by a singleton type with its currency symbol and the number of digits in the minor units, typically 0, 2, or 3, as parameters.
 """
-struct Cash{S, N} <: Instrument{S,Currency{S}} end
+struct Cash{S, N} <: Instrument{S,Currency{S}} 
+    Cash{S,N}() where {S,N} = unit(S) === N ? new{S,N}() : error("Currency minor unit does not match.")
+end
 Cash(S::Symbol) = Cash{S,unit(S)}()
 Cash(::Type{Currency{S}}) where {S} = Cash(S)
 Cash(::Currency{S}) where {S} = Cash(S)
@@ -45,14 +46,6 @@ end
 struct Stock{S,C} <: Instrument{S,Currency{C}}
     Stock(S::Symbol) = new{S,:USD}()
     Stock(S::Symbol,C::Symbol) = new{S,C}()
-end
-
-"""
-`ListedStock` is an implementation of `Instrument` represented by a singleton type with the exchange it's listed, its symbol and its currency, e.g. `ListedStock{:NASDAQ,:MSFT,:USD}`. The currency can be omitted and it will default to USD, e.g. `ListedStock(:NASDAQ,:MSFT)`.
-"""
-struct ListedStock{E,S,C} <: Instrument{S,Currency{C}}
-    ListedStock(E::Symbol, S::Symbol) = new{E,S,:USD}()
-    ListedStock(E::Symbol, S::Symbol, C::Symbol) = new{E,S,C}()
 end
 
 # Set up short names for all of the currencies (as instances of the Cash instruments). This is really done as a convenience
